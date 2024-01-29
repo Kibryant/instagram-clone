12863b2dffbea856c0e75c210adbabeb8d80a631
Kibryant <arthurgustavon@gmail.com>
2024-01-29 18:52:55 -0300

    chore(deps): add lucide-react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 const Page: React.FC = () => {
-  return <div>Page</div>
+  return (
+    <>
+      <header>
+        <div className="flex items-center justify-between p-4">
+          <div>
+            <h1 className="text-2xl font-semibold">Instagram</h1>
+          </div>
+          <div>
+            <button className="text-blue-500">Sign Up</button>
+            <button className="text-blue-500">Log In</button>
+          </div>
+        </div>
+      </header>
+    </>
+  )
 }
 
 export default Page
